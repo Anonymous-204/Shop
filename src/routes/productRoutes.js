2e40cdf227a1d658f066f9f77cc29acce15a4f75
Anonymous-204 <nladuong.2004@gmail.com>
2026-01-03 22:10:29 +0700

    thêm FE search

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,16 +2,15 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productControllers')
 const protectedRoute = require('../middlewares/authMiddleware');
-//=================== PUBLIC ===================//
+
 router.get('/search', productController.searchProducts);
 router.get('/all', productController.getAllPublicProduct);
 router.get('/meta',protectedRoute, productController.getProductMeta);
-router.get('/:productId', productController.getPublicProductDetail);
-//=================== PRIVATE ==================//
-router.use(protectedRoute)
-router.get('/products', productController.getProductByuserId);
-router.post('/create', productController.createProducts);
+
+router.get('/products',protectedRoute, productController.getProductByuserId);
+router.post('/create',protectedRoute, productController.createProducts);
 //=================== DYNAMIC (ĐỂ CUỐI) =======//
-router.patch('/:productId', productController.updateProducts);
-router.delete('/:productId', productController.deleteProductByProductId);
+router.patch('/:productId',protectedRoute, productController.updateProducts);
+router.delete('/:productId',protectedRoute, productController.deleteProductByProductId);
+router.get('/:productId', productController.getPublicProductDetail);
 module.exports = router;
